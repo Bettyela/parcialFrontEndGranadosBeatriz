f9cc90705c497398a41f921877b1f6e29d6460f2
Bettyela <beatriz.cecilia.granados@hotmail.com>
2023-05-31 16:44:17 -0500

    subiendo archivo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Componentes/Card'
+import Form from './Components/Form'
+
+function App() {
+  return (
+    <>
+      <Card/>
+      <Form/>           
+    </>
+  )
+}
+
+export default App
